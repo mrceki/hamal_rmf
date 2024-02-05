f2b741484d09c35fdea3a5f48b7bee2aa71c4e72
mrceki <105711013+mrceki@users.noreply.github.com>
2024-02-05 13:31:35 +0000

    remove unused members

--- rmf_hamal/include/rmf_hamal/hamal_task_dispatcher.hpp
+++ rmf_hamal/include/rmf_hamal/hamal_task_dispatcher.hpp
@@ -3,28 +3,19 @@
 
 #include "rclcpp/rclcpp.hpp"
 
-#include "rmf_task_msgs/srv/submit_task.hpp"
 #include "rmf_task_msgs/msg/bid_response.hpp"
 #include "rmf_fleet_msgs/msg/mode_request.hpp"
 #include "rmf_dispenser_msgs/msg/dispenser_request.hpp"
-#include "rmf_dispenser_msgs/msg/dispenser_result.hpp"
 #include "rmf_ingestor_msgs/msg/ingestor_request.hpp"
-#include "rmf_ingestor_msgs/msg/ingestor_result.hpp"
-
 class HamalTaskDispatcherNode : public rclcpp::Node
 {
 public:
   HamalTaskDispatcherNode();
 
 private:
-  void dispatch_task_callback(
-        const std::shared_ptr<rmf_task_msgs::srv::SubmitTask::Request> request,
-        const std::shared_ptr<rmf_task_msgs::srv::SubmitTask::Response> response);
   void bid_response_callback(const rmf_task_msgs::msg::BidResponse::SharedPtr msg);
   void dispenser_request_callback(const rmf_dispenser_msgs::msg::DispenserRequest::SharedPtr msg);
   void ingestor_request_callback(const rmf_ingestor_msgs::msg::IngestorRequest::SharedPtr msg);
-  void dispenser_result_callback(const rmf_dispenser_msgs::msg::DispenserResult::SharedPtr msg);
-  void ingestor_result_callback(const rmf_ingestor_msgs::msg::IngestorResult::SharedPtr msg);
 
   struct task_allocation
   {
